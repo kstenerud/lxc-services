87c47da5ed2ff3b1d875da24a1d1eb72eee4817f
Karl Stenerud <kstenerud@gmail.com>
2018-04-18 15:58:26 +0200

    Script fixups

--- fileshare/create_fileshare.sh
+++ fileshare/create_fileshare.sh
@@ -8,12 +8,15 @@
 
 set -eu
 
+SCRIPT_HOME=$(readlink -f "$(dirname "${BASH_SOURCE[0]}")")
+
 # Configuration
 CONTAINER_NAME=fileshare
-SHARED_DIRECTORY=/var/log
+SHARED_DIRECTORY=/mnt/shared
 
 # Base image
 lxc launch images:alpine/3.7 $CONTAINER_NAME
+lxc exec $CONTAINER_NAME -- sed -i 's/^tty/#tty/g' /etc/inittab
 
 # Privileged container (for NFS)
 lxc config set $CONTAINER_NAME security.privileged true
@@ -24,19 +27,19 @@ mount fstype=rpc_pipefs,"
 lxc restart $CONTAINER_NAME
 
 # Shared directory
-lxc exec $CONTAINER_NAME mkdir /mnt/shared
+lxc exec $CONTAINER_NAME -- mkdir /mnt/shared
 lxc config device add $CONTAINER_NAME shared disk source="$SHARED_DIRECTORY" path=/mnt/shared
 
 # Install software (need to sleep so that apk doesn't barf)
 sleep 1
-lxc exec $CONTAINER_NAME apk add samba nfs-utils
-pushd fs
+lxc exec $CONTAINER_NAME -- apk add samba nfs-utils
+pushd "$SCRIPT_HOME/fs"
 tar cf - . | lxc exec $CONTAINER_NAME -- tar xf - -C /
 popd
 
 # Services
-lxc exec $CONTAINER_NAME rc-update add samba
-lxc exec $CONTAINER_NAME rc-service samba start
-lxc exec $CONTAINER_NAME rc-update add nfs
-lxc exec $CONTAINER_NAME rc-service nfs start
+lxc exec $CONTAINER_NAME -- rc-update add samba
+lxc exec $CONTAINER_NAME -- rc-service samba start
+lxc exec $CONTAINER_NAME -- rc-update add nfs
+lxc exec $CONTAINER_NAME -- rc-service nfs start
 
